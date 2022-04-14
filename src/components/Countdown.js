abd2b9f73d2475ed91d93e2e66b687e18a5501c8
Manuel Berger <manuel.berger@codecentric.de>
2022-04-15 01:35:11 +0200

    chore: improve countdown, add jest testing library and add countdown unit tests

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,26 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import moment from "moment";
 
-const targetTime = moment("2022-09-10");
+export const toCountdown = ({ from = moment(), until }) => {
+  if (!until) return "";
+  const targetTime = moment(until);
+  const timeBetween = moment.duration(targetTime.diff(from));
+  return `${timeBetween.months()} Months ${timeBetween.days()} Days ${timeBetween.hours()} Hours`;
+};
 
-const Countdown = ({ data }) => {
-  const [currentTime, setCurrentTime] = useState(moment());
-  const timeBetween = moment.duration(targetTime.diff(currentTime));
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(moment());
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
+const Countdown = () => {
   return (
     <>
       <h3 className="counter has-text-centered" style={{ fontSize: "30px" }}>
-        <span>{timeBetween.months()} Months </span>
-        <span>{timeBetween.days()} Days </span>
-        <span>{timeBetween.hours()} Hours </span>
+        <span>{toCountdown("2022-09-10")}</span>
       </h3>
     </>
   );
---
+++ src/components/Countdown.unit.spec.js
@@ -0,0 +1,26 @@
+import { toCountdown } from "./Countdown";
+import moment from "moment";
+
+describe("toCountdown", () => {
+  test("returns a valid time information given a from and until date", () => {
+    expect(toCountdown({ from: "2022-04-16", until: "2022-09-10" })).toEqual(
+      "4 Months 25 Days 0 Hours"
+    );
+  });
+
+  test("returns a valid time information given only an until date", () => {
+    const expected = toCountdown({ from: moment(), until: "2022-09-10" });
+
+    expect(toCountdown({ until: "2022-09-10" })).toEqual(expected);
+  });
+
+  test("returns a negative time information given a from date in the future", () => {
+    expect(toCountdown({ from: "2022-10-01", until: "2022-09-10" })).toEqual(
+      "0 Months -21 Days 0 Hours"
+    );
+  });
+
+  test("returns an empty string given no until date", () => {
+    expect(toCountdown({})).toEqual("");
+  });
+});
